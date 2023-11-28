dd8a726c03338e4feaf67dcc1e5212835743b2a2
Guilherme Souza <guilherme.souza190@outlook.com>
2023-11-28 09:48:54 +0000

    feat: :zap: Criação de tabs aplicação

---
+++ app/financas/index.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+export default function FinancasScreen() {
+  return (
+    <View>
+      <Text>FinancasScreen</Text>
+    </View>
+  )
+}
---
+++ app/index.js
@@ -0,0 +1,5 @@
+import { Text } from 'react-native';
+import MyTabs from "../components/tabsScreen";
+export default function App() {
+  return <MyTabs />;
+}
